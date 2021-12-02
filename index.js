3be261c090fcdf116b6cb0868bf8aac908c46f71
Sneha <sneha.adlakha@gmail.com>
2021-12-02 07:15:26 -0800

    testing

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ let port=process.env.PORT||3000;
 app.get("/",(req,res)=>{
     res.send("HELLo World")
 });
-
-
+app.get("/users",(res,res)=>{
+    res.send("Users Axxessed")
+})
 app.listen(port,()=>{
     console.log(`Server Started at Port ${port}`);
 });
